657b6f85b14aa591d2e2c69214720ee960d76f06
Joern Turner <joern@existsolutions.com>
2024-02-11 22:39:36 +0100

    polling interval

--- src/nano.js
+++ src/nano.js
@@ -16,7 +16,7 @@ export class Nano {
             {
                 name:'smartthings',
                 url: `https://api.smartthings.com/v1/devices/${tokens.smartthings.device}/status`,
-                interval: 1000 * 60,
+                interval: 1000 * 60 * 5,
                 token: tokens.smartthings.token,
             },
             {
