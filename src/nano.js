df5b08a42cf547ddaa07cf3d3f4afab4c252a124
Joern Turner <joern@existsolutions.com>
2024-02-11 13:06:04 +0100

    refactored nano for more endpoint config

--- src/nano.js
+++ src/nano.js
@@ -14,17 +14,16 @@ export class Nano {
         }
         this.apiEndpoints = [
             {
+                name:'smartthings',
                 url: `https://api.smartthings.com/v1/devices/${tokens.smartthings.device}/status`,
                 interval: 1000 * 60 * 5,
                 token: tokens.smartthings.token,
             },
-/*
             {
-                url: 'https://api.example.com/endpoint2',
-                interval: 1500,
-                token: tokens.influxDB,
+                name: 'DWD-hourly-sunshine-duration-direct-diffuse',
+                url: 'https://api.open-meteo.com/v1/dwd-icon?latitude=52.4333&longitude=13.3076&hourly=sunshine_duration,direct_radiation_instant,diffuse_radiation_instant&timezone=Europe%2FBerlin',
+                interval: 1000 * 60 * 60,
             },
-*/
             // Add more endpoints as need
         ];
 
@@ -33,23 +32,42 @@ export class Nano {
 
     startPolling() {
         this.apiEndpoints.forEach(endpoint => {
+            this.request(endpoint);
+
             this.intervalIds[endpoint.url] = setInterval(() => {
-                this.fetchData(endpoint)
-                    .then(data => {
-                        this.extractData(data);
-                    })
+                console.log('fetching', endpoint.name);
+                this.request(endpoint);
+                /*
+                                this.fetchData(endpoint)
+                                    .then(data => {
+                                        this.extractData(data, endpoint);
+                                    })
+                */
             }, endpoint.interval);
         });
+    }
+
+    request(endpoint) {
+        this.fetchData(endpoint)
+            .then(data => {
+                this.extractData(data, endpoint);
+            })
     }
 
     async fetchData(endpointConfig) {
         console.log('fetchData', endpointConfig.url, endpointConfig.interval);
         try {
-            const response = await axios.get(endpointConfig.url, {
-                headers: {
-                    'Authorization': `${tokens.smartthings.token}`
-                }
-            })
+            let response;
+            if (endpointConfig.token) {
+                response = await axios.get(endpointConfig.url, {
+                    headers: {
+                        'Authorization': `${tokens.smartthings.token}`
+                    }
+                })
+            } else {
+                // anonymous API requires no token
+                response = await axios.get(endpointConfig.url, {})
+            }
             return response.data;
         } catch (error) {
             if (error.response) {
@@ -65,31 +83,50 @@ export class Nano {
         }
     }
 
-    async extractData(data) {
+    async extractData(data, endpoint) {
         // ?components?INDOOR2?temperatureMeasurement?temperature?value
         // console.log('extractAndStoreData.responseData', data)
 
-        const heating = data.components.INDOOR2.temperatureMeasurement.temperature.value || 'N/A';
+        if (endpoint.name === 'smartthings') {
+            const heating = data.components.INDOOR2.temperatureMeasurement.temperature.value || 'N/A';
+            const warmwater = data.components.main.temperatureMeasurement.temperature.value || 'N/A';
+            const energy = data.components.main.powerConsumptionReport.powerConsumption.value.power || 'N/A';
+            await this.writeData('nano','temperature', 'component', 'heating', 'temp', heating);
+            await this.writeData('nano','temperature', 'component', 'warmwater', 'temp', warmwater);
+            await this.writeData('nano','energy', 'component', 'heatpump', 'power', energy);
+        }
 
-        // components?main?temperatureMeasurement?temperature?value
-        const warmwater = data.components.main.temperatureMeasurement.temperature.value || 'N/A';
+        if (endpoint.name === 'DWD-hourly-sunshine-duration-direct-diffuse') {
+            // console.log('pulling sunshine', data)
+            const times = data.hourly.time;
+            const durations = data.hourly.sunshine_duration;
+            const directs = data.hourly.direct_radiation_instant;
+            const diffuses = data.hourly.diffuse_radiation_instant;
+            // console.log('times', times);
+            // console.log('durations', durations);
+            for (let i = 0; i < times.length; i++) {
+                const time = times[i];
+                const duration = durations[i];
+                const direct = directs[i];
+                const diffuse = diffuses[i];
+                console.log(time,duration,direct,diffuse);
+                // await this.writeData('foo','sunshine', 'location', 'Berlin', 'duration', duration);
+                // await this.writeData('sunshine','sunshine', 'location', 'Berlin', 'direct', direct);
+                // await this.writeData('sunshine','sunshine', 'location', 'Berlin', 'diffuse', diffuse);
+            }
+        }
 
-        // components?main?powerConsumptionReport?powerConsumption?value?power
-        const energy = data.components.main.powerConsumptionReport.powerConsumption.value.power || 'N/A';
-
-        await this.writeData('temperature', 'component', 'heating', 'temp', heating);
-        await this.writeData('temperature', 'component', 'warmwater', 'temp', warmwater);
-        await this.writeData('energy', 'component', 'heatpump', 'power', energy);
     }
 
-    async writeData(measurement, tagName, tagValue, fieldName, fieldValue) {
+    async writeData(bucket, measurement, tagName, tagValue, fieldName, fieldValue) {
 
+        console.log('write to bucket', bucket);
         console.log('measurement', measurement);
         console.log('influxConf', this.influxConf);
         const url = this.influxConf.url;
         const token = tokens.influxDB;
         const org = this.influxConf.org;
-        const bucket = this.influxConf.bucket;
+        // const bucket = this.influxConf.bucket;
         /**
          * Instantiate the InfluxDB client
          * with a configuration object.
@@ -104,7 +141,7 @@ export class Nano {
         /**
          * Apply default tags to all points.
          **/
-        // writeApi.useDefaultTags({ region: 'west' })
+            // writeApi.useDefaultTags({ region: 'west' })
         const point1 = new Point(measurement)
                 .tag(tagName, tagValue)
                 .floatField(fieldName, fieldValue)
@@ -130,3 +167,4 @@ export class Nano {
 }
 
 new Nano().startPolling();
+// pullSunshine();
